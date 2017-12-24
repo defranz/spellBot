dcabbfa8f2485c70bdfa8db64fdf837791c2f66d
Derek Franz <defranz@indiana.edu>
2017-12-24 01:40:34 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {
-  consumer_key:         '8RVLFbcdlMwRgmrB731zQgT7B',
-  consumer_secret:      'OP7LsndidQiCAM6ZVjmUqtxKUt6TBCClcph3szVRqMljAzZrtK',
-  access_token:         '941555347780759557-Y1lze6KgfuuK6ABg8xxOPmAxdhrpISs',
-  access_token_secret:  'ajF68eiHHsJekFVmMP8lLtx34QVDfpsmt8D9yhbiyDOLw'
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  ''
 }
 
 module.exports = config;
